b742b7d389e8f659cdbd7c7596afbc8e96f5f01a
Bryan D Alvarez Pandozi <bdap121299@gmail.com>
2026-04-26 19:38:04 -0500

    added sorting functions to recipients page

--- public/scripts/allRecipients.js
+++ public/scripts/allRecipients.js
@@ -30,3 +30,63 @@ const buttons = document.getElementsByClassName("recipientSubscribeButton");
 for (const button of buttons) {
   button.addEventListener("click", updateRecipient);
 }
+
+// Sorting Logic
+//get all recipients
+//find current sort state
+//sort recipients by condition (opposite to current state)
+//take out recipients and replace them in correct order
+
+function getAllRecipientElements() {
+  return document.getElementsByClassName("recipient-identifier");
+}
+
+function findCurrentSortState() {
+  return 1;
+}
+
+function sortRecipientsList(recipients, sortingFn) {
+  return [...recipients].sort(sortingFn);
+}
+
+function replaceRecipientsList(recipients) {
+  const tableBody = document.getElementsByTagName("tbody")[0];
+  tableBody.replaceChildren(...recipients);
+}
+
+function sortRecipients(button, sortingFunction) {
+  let ascending = button.dataset.ascending === "true";
+  let recipients = getAllRecipientElements();
+  recipients = sortRecipientsList(recipients, (a, b) =>
+    ascending ? sortingFunction(a, b) : sortingFunction(b, a),
+  );
+  replaceRecipientsList(recipients);
+  button.dataset.ascending = (!ascending).toString();
+}
+
+function sortByName(button) {
+  const nameSorting = (a, b) => {
+    return a.children[0].textContent
+      .trim()
+      .localeCompare(b.children[0].textContent.trim());
+  };
+  sortRecipients(button, nameSorting);
+}
+
+function sortByEmail(button) {
+  const emailSorting = (a, b) => {
+    return a.children[1].textContent
+      .trim()
+      .localeCompare(b.children[1].textContent.trim());
+  };
+  sortRecipients(button, emailSorting);
+}
+
+function sortByStatus(button) {
+  const statusSorting = (a, b) => {
+    return a.children[2].textContent
+      .trim()
+      .localeCompare(b.children[2].textContent.trim());
+  };
+  sortRecipients(button, statusSorting);
+}
